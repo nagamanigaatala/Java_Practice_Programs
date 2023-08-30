5348a7238b481e8b9698b1c292cb896bb395c991
Nagamani Gaatala <57241062+nagamanigaatala@users.noreply.github.com>
2023-08-31 00:27:40 +0530

    Add files via upload

---
+++ Operators/Operators.java
@@ -0,0 +1,51 @@
+package com.Operators;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int i = 20;
+        int j = 6;
+         //Arithmatic Operators
+        /**System.out.println(i+j);
+        System.out.println(i-j);
+        System.out.println(i*j);
+        System.out.println(i/j); //cofficent
+        System.out.println(i%j); //remainder */
+        //Unary Operator
+        /**System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(--j);
+        System.out.println(j--);
+        System.out.println(j); */
+        //Relational Operators
+        /**System.out.println(i==j);
+        System.out.println(i!=j);
+        System.out.println(i<j);
+        System.out.println(i>j);
+        System.out.println(i>=j);
+        System.out.println(i<=j);*/
+        //Conditional Operator
+        /*
+         * &&
+         * t - t = t
+         * t - f = f
+         * f - t = f
+         * f - f = f
+         * ||
+         * t - t =t
+         * t - f = t
+         * f - t = t
+         * f - f = f 
+         */
+       /*System.out.println(i>j && i >= j);
+        System.out.println(i<j || i==j);*/
+        //Assigment Operators
+        /*i = i+10;
+        System.out.println(i);*/
+         i %= 5;
+        System.out.println(i);
+	}
+}
---
+++ Operators/Operators_Practice.java
@@ -0,0 +1,12 @@
+package com.Operators;
+
+public class Operators_Practice {
+
+	public static void main(String[] args) {
+		 int i = 10;
+	        int j = 5;
+	        int n = i++ + ++i + i-- - --i + ++i;
+	        System.out.println(n);
+	}
+
+}
