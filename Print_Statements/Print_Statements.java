4a06754ee839eb28a0cca7a908069d7c5cadc4b6
Nagamani Gaatala <57241062+nagamanigaatala@users.noreply.github.com>
2023-08-31 00:28:31 +0530

    Add files via upload

---
+++ Print_Statements/Print_Statements.java
@@ -0,0 +1,16 @@
+package com.Print_Statements;
+
+public class Print_Statements {
+
+	public static void main(String[] args) {
+		 //print, printf, println
+        /**System.out.print("hello nobichan");
+        System.out.println("how are you man");*/
+        //printf
+        //System.out.printf("the current year is %d and the current month is %s", 2023,"june");
+        //err
+        System.err.print("nobi");
+
+	}
+
+}
